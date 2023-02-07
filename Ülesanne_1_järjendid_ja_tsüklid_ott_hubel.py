d5855ebf43c664aea6e1e75f74c8d69e3504d84e
ott42 <115535234+ott42@users.noreply.github.com>
2023-02-07 10:18:15 +0200

    Update Ülesanne_1_järjendid_ja_tsüklid_ott_hubel.py

--- Ülesanne_1_järjendid_ja_tsüklid_ott_hubel.py
+++ Ülesanne_1_järjendid_ja_tsüklid_ott_hubel.py
@@ -1,5 +1,6 @@
 linnad = ["Võhma", "Haapsalu", "Jõhvi", "Kuresaare", "Pärnu", "Rakvere", "Tallinn"]
 linnad.sort(reverse=False)
 linnad_pikkus = (len(linnad))
-print(linnad_pikkus)
 print(linnad)
+print("Linnasi on: ")
+print(linnad_pikkus)
